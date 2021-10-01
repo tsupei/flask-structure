40f5d34fa5bd9322fb78b46483fb15ec565ae108
TsuPei <tsupei0527@gmail.com>
2021-10-01 15:18:26 +0000

    stock linebot base

--- api/__init__.py
+++ api/__init__.py
@@ -1,1 +1,2 @@
 from api.hello_api import hello_api
+from api.chatbot_api import chatbot_api
--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask_cors import CORS
-from api import hello_api
+from api import hello_api, chatbot_api
 
 
 def main():
@@ -11,14 +11,15 @@ def main():
 
     # Settings
     app.config["ip"] = "127.0.0.1"
-    app.config["port"] = "47000"
+    app.config["port"] = "47001"
     # app.config["db"] =
     # app.config["env"] =
     # ...
 
-    prefix = "/{}/{}/{}".format("test", "api", "v1")
+    prefix = "/{}/{}/{}".format("chatbot", "api", "v1")
     app.register_blueprint(hello_api, url_prefix=prefix)
-    app.run(host="127.0.0.1", port="47000", threaded=True)
+    app.register_blueprint(chatbot_api, url_prefix=prefix)
+    app.run(host="127.0.0.1", port="47001", threaded=True)
 
 
 if __name__ == "__main__":
